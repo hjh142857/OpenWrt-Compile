e278e1046baf3e3228e84fd11d59c6a5b7649198
LittlePri <23516992+hjh142857@users.noreply.github.com>
2021-04-06 18:46:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,26 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# DIY package
+mkdir package/diy
+
+mkdir package/kenzo
+git clone https://github.com/kenzok8/openwrt-packages package/kenzo
+mkdir package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+mv package/kenzo/luci-app-adguardhome package/diy/
+mv package/kenzo/AdGuardHome package/diy/
+mv package/kenzo/luci-app-eqos package/diy/
+mv package/kenzo/luci-theme-argon_new package/diy/
+mv package/OpenAppFilter/luci-app-oaf package/diy/
+mv package/OpenAppFilter/oaf package/diy/
+mv package/OpenAppFilter/open-app-filter package/diy/
+
+rm -rf package/kenzo
+rm -rf package/OpenAppFilter
